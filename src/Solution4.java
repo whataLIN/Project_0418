a20347a6e8f4ce25c7e23e45edabd17a2f17bb2c
whataLIN <kitschh@naver.com>
2023-04-19 15:21:23 +0900

    solution4

---
+++ src/Solution4.java
@@ -0,0 +1,10 @@
+import java.text.MessageFormat;
+import java.util.Arrays;
+
+public class Solution4 {
+    public static void main(String[] args) {
+        // array : 0개 이상의 같은 타입을 가지는 값의 묶음
+
+
+    }
+}

c8c4e64d0d7d54b3c8b4a563426d11c9917666e7
whataLIN <kitschh@naver.com>
2023-04-19 11:55:47 +0900

    Initial commit

---
+++ src/Solution1.java
@@ -0,0 +1,18 @@
+public class Solution1 {
+    public static void main(String[] args) {
+        System.out.println("헤의~");
+
+        // 들여쓰기 중요 x. 세미콜론 필요
+
+        int a=10;
+        long b = 100000000000000000L;
+        System.out.println(a);
+        System.out.println(b);
+
+        float c = 3.14F;
+        double d = 3.14;
+        // 뒤에 타입 나타내는 대소문자 붙을 시 default로 처리하지 않음
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
---
+++ src/Solution2.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        //boolean in java
+        boolean b = true;
+        //        b=1
+        boolean c = false;
+        System.out.println(c);
+        System.out.println(d);
+
+        //var는 선언에 쓸 수 없음 - 초기화는 가능
+
+
+    }
+}
